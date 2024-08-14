563a3fdcb08f09d90121b744db10e16211554fa9
Edward <edwardapersil7@gmail.com>
2024-08-14 17:31:47 +0000

    initial commit

---
+++ src/main/java/com/ecobank/intern_portal/InternPortalApplication.java
@@ -0,0 +1,13 @@
+package com.ecobank.intern_portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternPortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecobank/intern_portal/InternPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecobank.intern_portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
